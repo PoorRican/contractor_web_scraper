1c1eff4875ecc3ea010df59ec110803e9f7941f1
Josue Figueroa <poor.rican@pm.me>
2023-10-29 05:10:25 -0700

    Refactor `AddressScraper` to a template `TextSnippetScraper`. Keep concrete `AddressScraper` implementation.

--- parsers/AddressScraper.py
+++ parsers/AddressScraper.py
@@ -1,101 +1,40 @@
-import re
-import warnings
-from copy import copy
-from typing import ClassVar, Union, Callable, NoReturn
+from typing import ClassVar
 
-from bs4 import Tag, PageElement
 from langchain.prompts import PromptTemplate
 from langchain.schema.runnable import Runnable
-from openai import InvalidRequestError
 
 from llm import LONG_MODEL_PARSER
-from models import Contractor
-
-_address_scaper_prompt = PromptTemplate.from_template(
-    """You will be given the HTML content of a construction company website.
-    
-    Here is the content: ```{content}```
-    
-    What is the mailing address of the company? Return only the address and nothing else.
-    If there are two addresses, return the first one, but nothing else.
-    If there is no mailing address within the content, return 'no address' and nothing else.
-    """
-)
-
-_formatter_prompt = PromptTemplate.from_template(
-    """You will be given text that should directly represent a mailing address.
-    
-    Here is the address: {address}
-    
-    Is this a specific mailing address? If not, return 'no address' and nothing else.
-    Format the mailing address so that it is on one line, with commas separating each part of the address.
-    """
-)
+from parsers.TextSnippetScraper import TextSnippetScraper
 
 
-class AddressScraper:
-    _extract_chain: ClassVar[Runnable] = _address_scaper_prompt | LONG_MODEL_PARSER
-    _chain: ClassVar[Runnable] = {'address': _extract_chain} | _formatter_prompt | LONG_MODEL_PARSER
+def _address_scraper_chain() -> Runnable:
+    """ Create a chain of LLM models to extract addresses from text snippets """
+    _address_scaper_prompt = PromptTemplate.from_template(
+        """You will be given the HTML content of a construction company website.
 
-    @staticmethod
-    def _format_address(address: str) -> str:
-        pattern = r'([A-Za-z0-9])\n'
-        stripped = re.sub(pattern, r'\1, ', address)
-        return stripped.replace(',\n', ', ').replace('.\n', ', ')
+        Here is the content: ```{content}```
 
-    @staticmethod
-    def _strip_extra_data(content: Tag) -> Tag:
-        for tag in content.find_all(True):
-            # Remove all HTML attributes from the tag
-            tag.attrs = {}
-        return content
-
-    async def _process(self, content: Union[Tag, PageElement]) -> Union[str, None]:
-        """ Attempt to extract address from HTML content
-
-        Parameters:
-            content: HTML content to extract address from
-
-        Returns:
-            Address string if found, else None
+        What is the mailing address of the company? Return only the address and nothing else.
+        If there are two addresses, return the first one, but nothing else.
+        If there is no mailing address within the content, return 'no address' and nothing else.
         """
-        try:
-            result: str = await self._chain.ainvoke({'content': str(content)})
-            if result.lower() != 'no address':
-                return self._format_address(result)
-                # replace newlines with commas using regex
-        except InvalidRequestError:
-            print(f"InvalidRequestError while scraping address. String might be too many tokens...")
-            # TODO: break down content into smaller pieces
-        return None
+    )
 
-    async def __call__(self, content: Tag, url: str, callback: Callable[[str], None]) -> NoReturn:
-        """ Scrape address from HTML content """
-        _content = self._strip_extra_data(copy(content))
+    _formatter_prompt = PromptTemplate.from_template(
+        """You will be given text that should directly represent a mailing address.
 
-        # attempt to find address in footer or header
-        for i in ('footer', 'header'):
-            section = _content.find(i)
-            if section is not None:
-                address = await self._process(section)
-                if address is not None:
-                    return callback(address)
+        Here is the address: {address}
 
-        # begin to look at all small text snippets
-        for i in ('p', 'span', 'a'):
-            sections = _content.find_all(i)
-            for section in sections:
-                address = await self._process(section)
-                if address is not None:
-                    return callback(address)
+        Is this a specific mailing address? If not, return 'no address' and nothing else.
+        Format the mailing address so that it is on one line, with commas separating each part of the address.
+        """
+    )
 
-        # as a last resort, look at first and last chunks
-        chunk_size = 5000
-        first, last = str(_content)[:chunk_size], str(_content)[-chunk_size:]
-        for i in (first, last):
-            address = await self._process(i)
-            if address is not None:
-                return callback(address)
+    _address_extract_chain: Runnable = _address_scaper_prompt | LONG_MODEL_PARSER
+    return {'address': _address_extract_chain} | _formatter_prompt | LONG_MODEL_PARSER
 
-        warnings.warn(f"Could not find address on site: {url}")
-        return
+
+class AddressScraper(TextSnippetScraper):
+    _chain: ClassVar[Runnable] = _address_scraper_chain()
+    _failure_text: ClassVar[str] = 'no address'
+    _search_type: ClassVar[str] = 'address'
---
+++ parsers/TextSnippetScraper.py
@@ -0,0 +1,111 @@
+import warnings
+from abc import abstractmethod, ABC
+from copy import copy
+from typing import Union, Callable, NoReturn
+
+from bs4 import Tag, PageElement
+from langchain.schema.runnable import Runnable
+from openai import InvalidRequestError
+
+
+class TextSnippetScraper(ABC):
+    """ A template functor class for scraping text snippets from HTML content.
+
+    This class is designed for extracting small text snippets from HTML content. It begins by looking at the header and
+    footer of the HTML content, then looks at all small text snippets (<p>, <span>, <a>). Finally, it then chunks takes
+    large chunks of text from the beginning and end of the HTML content.
+
+    Subclasses should override the following properties:
+        _chain: a chain of LLM models to extract data from text snippets
+        _failure_text: a string to be returned by LLM when no data is found
+        _search_type: a string representing the type of data being scraped. Used for user feedback.
+    """
+    @property
+    @abstractmethod
+    def _chain(self) -> Runnable:
+        """ Return a chain of LLM models to extract data from text snippets.
+
+        This should be overridden by subclasses.
+        """
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def _failure_text(self) -> str:
+        """ Return a string to be returned by LLM when no data is found.
+
+        ie: 'no address', 'no phone number', etc.
+
+        This should be overridden by subclasses.
+        """
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def _search_type(self) -> str:
+        """ Return a string representing the type of data being scraped.
+
+        ie: 'address', 'phone number', etc.
+
+        This should be overridden by subclasses and is used for user feedback.
+        """
+        raise NotImplementedError
+
+    @staticmethod
+    def _strip_extra_data(content: Tag) -> Tag:
+        """ Preprocess HTML content to remove all HTML attributes from tags.
+
+        This is used to remove all superfluous data and significantly reduce the number of tokens in the content.
+        """
+        for tag in content.find_all(True):
+            tag.attrs = {}
+        return content
+
+    async def _process(self, content: Union[Tag, PageElement]) -> Union[str, None]:
+        """ Attempt to extract the text snippet from HTML content using `self._chain`.
+
+        Parameters:
+            content: HTML content to extract snippet from
+
+        Returns:
+            The extracted snippet, or None if no snippet was found.
+        """
+        try:
+            result: str = await self._chain.ainvoke({'content': str(content)})
+            if result.lower() != self._failure_text:
+                return result
+        except InvalidRequestError:
+            print(f"InvalidRequestError while scraping {self._search_type}. String might be too many tokens...")
+            # TODO: break down content into smaller pieces
+        return None
+
+    async def __call__(self, content: Tag, url: str, callback: Callable[[str], None]) -> NoReturn:
+        """ Scrape snippet from HTML content """
+        _content = self._strip_extra_data(copy(content))
+
+        # attempt to find snippet in footer or header
+        for i in ('footer', 'header'):
+            section = _content.find(i)
+            if section is not None:
+                snippet = await self._process(section)
+                if snippet is not None:
+                    return callback(snippet)
+
+        # begin to look at all small text snippets
+        for i in ('p', 'span', 'a'):
+            sections = _content.find_all(i)
+            for section in sections:
+                snippet = await self._process(section)
+                if snippet is not None:
+                    return callback(snippet)
+
+        # as a last resort, look at first and last chunks
+        chunk_size = 5000
+        first, last = str(_content)[:chunk_size], str(_content)[-chunk_size:]
+        for i in (first, last):
+            snippet = await self._process(i)
+            if snippet is not None:
+                return callback(snippet)
+
+        warnings.warn(f"Could not find {self._search_type} on site: {url}")
+        return
